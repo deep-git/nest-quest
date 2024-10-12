74dfde9950896cb372565caee3d695cdbc9e319c
deep-git <deepakthangella5@gmail.com>
2024-10-11 23:15:06 -0400

    test

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -5,6 +5,8 @@ export const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
     dotenv.config();
 
+    console.log("VERIFY TOKEN: ", token);
+
     if (!token) {
         return res.status(401).json({ message: "Not authenticated!" });
     }
